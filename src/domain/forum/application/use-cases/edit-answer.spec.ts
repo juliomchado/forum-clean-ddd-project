1ba2c6e063f2dc10ced1463c688717f60893d1cf
Julio Machado <juliocarlos00@hotmail.com>
2025-03-26 22:17:13 +0000

    add: edit answer + question response

---
+++ src/domain/forum/application/use-cases/edit-answer.spec.ts
@@ -0,0 +1,55 @@
+import { InMemoryAnswersRepository } from "test/repositories/in-memory-answers-repository";
+import { makeAnswer } from "test/factories/make-answer";
+import { UniqueEntityID } from "@/core/entities/unique-entitiy-id";
+import { EditAnswerUseCase } from "./edit-answer";
+
+let inMemoryAnswersRepository: InMemoryAnswersRepository;
+let sut: EditAnswerUseCase;
+
+describe("Edit answer", () => {
+  beforeEach(() => {
+    inMemoryAnswersRepository = new InMemoryAnswersRepository();
+    sut = new EditAnswerUseCase(inMemoryAnswersRepository);
+  });
+
+  test("should be able to edit a answer", async () => {
+    const { answer } = makeAnswer(
+      {
+        authorId: new UniqueEntityID("author-id"),
+      },
+      new UniqueEntityID("answer-1")
+    );
+
+    await inMemoryAnswersRepository.create(answer);
+
+    await sut.execute({
+      answerId: answer.id.toString(),
+      authorId: "author-id",
+      content: "edited content",
+    });
+
+    expect(inMemoryAnswersRepository.items[0]).toMatchObject({
+      content: "edited content",
+    });
+    expect(inMemoryAnswersRepository.items).toHaveLength(1);
+  });
+
+  test("should not be able to edit a answer from another user", async () => {
+    const { answer } = makeAnswer(
+      {
+        authorId: new UniqueEntityID("author-id-1"),
+      },
+      new UniqueEntityID("answer-1")
+    );
+
+    await inMemoryAnswersRepository.create(answer);
+
+    await expect(
+      sut.execute({
+        answerId: "answer-1",
+        authorId: "author-id-2",
+        content: "edited content",
+      })
+    ).rejects.toBeInstanceOf(Error);
+  });
+});
