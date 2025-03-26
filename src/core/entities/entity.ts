dec2e4ea2681e230025e90c95c1fb73d1a15dc6e
Julio Machado <juliocarlos00@hotmail.com>
2025-03-26 00:31:40 +0000

    add: entities + value objects

---
+++ src/core/entities/entity.ts
@@ -0,0 +1,16 @@
+import { UniqueEntityID } from "./unique-entitiy-id";
+
+export class Entity<T> {
+  private _id: UniqueEntityID;
+
+  protected props: T;
+
+  get id() {
+    return this._id;
+  }
+
+  constructor(props: T, id?: string) {
+    this.props = props;
+    this._id = new UniqueEntityID(id);
+  }
+}
